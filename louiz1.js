c9ef432dd1bf3b7164e0410133c861a8d4aeb92d
wezlou <louizmadrona509@gmail.com>
2024-12-19 16:12:17 +0800

    initial commit

---
+++ louiz1.js
@@ -0,0 +1,15 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    if (name && email && message) {
+      alert('Message sent successfully!');
+    
+    } else {
+      alert('Please fill in all fields');
+    }
+  });
+  
